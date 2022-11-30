fc5170c501a187505ec405df64a361d039a95e45
tuomasme <mertanentuomas@gmail.com>
2022-11-30 19:55:19 +0200

    Bootstrap buttons

--- pages/table_page.py
+++ pages/table_page.py
@@ -22,8 +22,8 @@ fig = px.imshow(rgb_data)
 fig.update_layout(FIGURE_STYLE)
 image = dcc.Graph(figure=fig,
                   style={'width': '40vh', 'height': '40vh'})
-imageButton = html.Button(id='btn', children=f'Image ID : {id}',
-                                style={'margin-left': 100, 'width': 150})
+imageButton = dbc.Button(id='btn', children=f'Image ID : {id}', className='me-2',
+                                style={'margin-left': 100, 'width': 150, 'margin-right': 10000})
 
 imageLabel = html.H5(f'Image ID : {id}',
                                 style={'margin-left': 100, 'color': 'white'})
@@ -45,7 +45,7 @@ delete_modify_layout = html.Div(children=[
                 ]) for i in range(5)
                 ], style={"border": "2px white transparent",
                       'margin': 10, 'border-radius': 10, 'background': '#181240',
-                      'width': 6}),
+                      'width': 6, 'vertical-align': 'top'}),
 
             dbc.Col(
                 [
@@ -59,14 +59,14 @@ delete_modify_layout = html.Div(children=[
                     html.H6(children=f'Image ID : {id}',
                             style={'color': 'white', 'margin-left': 90, 'margin-bottom': 10}),
                     dcc.Input(id="inputImageId", type="text", value='', style={'margin-left': 90, 'margin-bottom': 10}),
-                    html.Button('Update', id='btnUpdateImageId', style={'margin-left': 20}),
+                    dbc.Button('Update', id='btnUpdateImageId', style={'margin-left': 20}),
                     html.H6(children=f'Patient ID : {id}',
                             style={'color': 'white', 'margin-left': 90, 'margin-bottom': 10 }),
                     dcc.Input(id="inputPatientId", type="text", placeholder="", style={'margin-left': 90, 'margin-bottom': 10}),
-                    html.Button('Update', id='btnUpdatePatientId', style={'margin-left': 20}),
+                    dbc.Button('Update', id='btnUpdatePatientId', style={'margin-left': 20}),
                     html.Br(),
                     html.Br(),
-                    html.Button('Delete', id='btnDelete', style={'margin-left': 90, 'margin-bottom': 10})
+                    dbc.Button('Delete', id='btnDelete', style={'margin-left': 90, 'margin-bottom': 10})
                 ], style={"border": "2px white transparent",
                           'margin': 10, 'border-radius': 10, 'background': '#181240',
                           'width': 3}, align='top'),
